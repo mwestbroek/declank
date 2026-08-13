998e9941b0953040e4b77b69f6e7b7ced74f85bf
Maurits Westbroek <maurits@hoxtonfarms.com>
2026-08-13 15:53:58 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd engine
+wasm-pack build --target no-modules
+cp pkg/declank_engine.js ../extension/
+{
+  printf 'self.DECLANK_WASM_B64 = "'
+  base64 < pkg/declank_engine_bg.wasm | tr -d '\n'
+  printf '";\n'
+} > ../extension/declank_wasm.js
+
+cd ..
